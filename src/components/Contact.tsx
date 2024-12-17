74645b03c353125393943b15bdc3ddbe357c27f1
Alexandru <a.muresan8533@gmail.com>
2024-12-16 21:30:22 -0500

    created contact form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Container } from '@/components/Container';
+import { PaperAirplaneIcon } from '@heroicons/react/24/outline';
+import Button from './Button';
 
 export const Contact = () => {
 	return (
-		<Container id='contact'>
-			<div className='flex flex-wrap items-center justify-between w-full max-w-4xl gap-5 mx-auto text-white bg-indigo-600 px-7 py-7 lg:px-12 lg:py-12 lg:flex-nowrap rounded-xl'>
-				<div className='flex-grow text-center lg:text-left'>
+		<Container>
+			<div className='flex flex-col md:flex-row items-center justify-between w-full max-w-5xl gap-5 mx-auto text-white bg-indigo-600 px-7 py-7 lg:px-12 lg:py-12 lg:flex-nowrap rounded-xl'>
+				<div className='flex-1 text-center lg:text-left'>
 					<h2 className='text-2xl font-medium lg:text-3xl'>
 						Ready for a Cleaner Space?
 					</h2>
@@ -14,16 +16,65 @@ export const Contact = () => {
 						eco-friendly cleaning solutions.
 					</p>
 				</div>
-				<div className='flex-shrink-0 w-full text-center lg:w-auto'>
-					<a
-						href='https://github.com/web3templates'
-						target='_blank'
-						rel='noopener'
-						className='inline-block py-3 mx-auto text-lg font-medium text-center text-indigo-600 bg-white rounded-md px-7 lg:px-10 lg:py-5 '
+				<form
+					className='flex-1 w-full flex flex-col gap-y-2 relative mt-6 md:mt-0 md:py-2 lg:py-0'
+					action='https://api.web3forms.com/submit'
+					method='POST'
+				>
+					<input
+						type='hidden'
+						name='access_key'
+						value='260f1943-с6а3-4a66-9830-e2c0cbbe033b'
+					></input>
+					<div className='flex flex-col md:flex-row gap-y-2 md:gap-x-2'>
+						<div className='relative flex-1'>
+							<input
+								name='form-name'
+								type='text'
+								required
+								className='w-full bg-white text-indigo-500 h-[40px] p-2 focus:outline-none rounded-md peer/name'
+							></input>
+							<label
+								htmlFor='form-name'
+								className='pointer-events-none absolute flex items-center left-2 top-0 text-md opacity-50 text-indigo-500 h-[40px] transition-all duration-300 ease-in-out peer-focus/name:top-[-32px] peer-focus/name:opacity-100 peer-focus/name:text-white peer-focus/name:left-0 peer-valid/name:top-[-32px] peer-valid/name:opacity-100 peer-valid/name:text-white peer-valid/name:left-0'
+							>
+								Name
+							</label>
+						</div>
+						<div className='relative flex-1'>
+							<input
+								name='form-email'
+								type='email'
+								required
+								autoComplete='none'
+								placeholder=' '
+								className='w-full bg-white text-indigo-500 h-[40px] p-2 focus:outline-none rounded-md peer/email transition-all ease-in-out duration-300 focus:mt-5 [&:not(:placeholder-shown)]:mt-5'
+							></input>
+							<label
+								htmlFor='form-name'
+								className='pointer-events-none absolute flex items-center left-2 top-0 text-md opacity-50 text-indigo-500 h-[40px] transition-all duration-300 ease-in-out peer-focus/email:top-[-12px] peer-focus/email:opacity-100 peer-focus/email:text-white peer-focus/email:left-0 peer-[:not(:placeholder-shown)]/email:top-[-12px] peer-[:not(:placeholder-shown)]/email:opacity-100 peer-[:not(:placeholder-shown)]/email:text-white peer-[:not(:placeholder-shown)]/email:left-0'
+							>
+								Email
+							</label>
+						</div>
+					</div>
+					<textarea
+						name='form-content'
+						placeholder='What services are you interested in?'
+						className='bg-white text-indigo-500 p-2 focus:outline-none rounded-md resize-none min-h-[150px] placeholder-indigo-500 placeholder-opacity-50'
+					></textarea>
+					<input
+						type='checkbox'
+						name='botcheck'
+						className='hidden'
+					></input>
+					<button
+						type='submit'
+						className='absolute bottom-1 right-1 flex items-center justify-center w-10 h-10 bg-indigo-700 text-white rounded-md'
 					>
-						Get a Quote Today
-					</a>
-				</div>
+						<PaperAirplaneIcon className='w-6 h-6' />
+					</button>
+				</form>
 			</div>
 		</Container>
 	);
