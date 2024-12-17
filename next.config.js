97da7065f2547ca8feda2800f87809b88522f667
Alexandru <a.muresan8533@gmail.com>
2024-12-16 21:56:43 -0500

    attempt to fix styles

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: 'export', // <=== enables static exports
-	images: {
-		unoptimized: process.env.NODE_ENV === 'development'
-	},
 	reactStrictMode: true,
-	basePath: process.env.NODE_ENV === 'development' ? '' : '/PerazasCleaning'
+	basePath: '/PerazasCleaning'
 };
 
 module.exports = nextConfig;
